1ad794b6a4f6ba5f6539e742b2a9d98ae6629ae5
Mohamed Ali Alkassas <elasasmohamed97@gmail.com>
2023-04-29 18:34:41 +0300

    Creating primitive files needed for this project. -scripts files -sass files -adding dist/ node_modules to .gitignore file.
    Design Landing page including:
      - logo
      - sign in button
      - main header

---
+++ scripts/vendors.js
@@ -0,0 +1,7 @@
+const signInBtn = document.getElementById("sign_in-btn");
+
+const signInFormSubmitBtn = document.getElementById("submit-btn");
+
+const signInForm = document.querySelector(".sign_in--form");
+
+const signInLayout = document.querySelector(".sign_in");
